e905db95af820a98a6349598c58ab42a7626f0fc
Pranay Kumar <65266203+gpranaykumar@users.noreply.github.com>
2025-01-10 10:57:34 +0530

    Update fetchRepoName.ps1

--- FetchRepoName/fetchRepoName.ps1
+++ FetchRepoName/fetchRepoName.ps1
@@ -1,2 +1,4 @@
-$repoName = $env:REPO_NAME
-Write-Host "The repository name is: $repoName"
+$repo = $env:REPO_NAME
+Write-Host "The repository  is: $repo"
+$repoName = ($repo.Split('/'))[1]
+Write-Host "The repository Name  is: $repoName"
